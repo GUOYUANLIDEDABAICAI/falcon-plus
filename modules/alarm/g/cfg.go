08214af7747357f308e57454f0d65926a0482143
果园里的大白菜 <33027643+GUOYUANLIDEDABAICAI@users.noreply.github.com>
2023-12-24 12:06:45 +0800

    Update cfg.go

--- modules/alarm/g/cfg.go
+++ modules/alarm/g/cfg.go
@@ -1,17 +1,3 @@
-// Copyright 2017 Xiaomi, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package g
 
 import (
@@ -38,12 +24,14 @@ type RedisConfig struct {
 }
 
 type ApiConfig struct {
-	Sms          string `json:"sms"`
-	Mail         string `json:"mail"`
-	Dashboard    string `json:"dashboard"`
-	PlusApi      string `json:"plus_api"`
-	PlusApiToken string `json:"plus_api_token"`
-	IM           string `json:"im"`
+	Sms           string `json:"sms"`
+	Mail          string `json:"mail"`
+	Dashboard     string `json:"dashboard"`
+	PlusApi       string `json:"plus_api"`
+	PlusApiToken  string `json:"plus_api_token"`
+	IM            string `json:"im"`
+	IMDingPrefix  string `json:"im_ding_prefix"`
+	AlarmCallback string `json:"alarm_callback"`
 }
 
 type FalconPortalConfig struct {
@@ -58,15 +46,9 @@ type WorkerConfig struct {
 	Mail int `json:"mail"`
 }
 
-type AlarmChannelConfig struct {
-	Enabled bool   `json:"enabled"`
-	AMApi   string `json:"alarm_manager_api"`
-}
-
 type HousekeeperConfig struct {
-	Enabled            bool `json:"enabled"`
-	EventRetentionDays int  `json:"event_retention_days"`
-	EventDeleteBatch   int  `json:"event_delete_batch"`
+	EventRetentionDays int `json:"event_retention_days"`
+	EventDeleteBatch   int `json:"event_delete_batch"`
 }
 
 type GlobalConfig struct {
@@ -75,7 +57,6 @@ type GlobalConfig struct {
 	Http         *HttpConfig         `json:"http"`
 	Redis        *RedisConfig        `json:"redis"`
 	Api          *ApiConfig          `json:"api"`
-	AlarmChannel *AlarmChannelConfig `json:"alarm_channel"`
 	Worker       *WorkerConfig       `json:"worker"`
 	Housekeeper  *HousekeeperConfig  `json:"Housekeeper"`
 }
